fdf5cb656848ab2f5236e3ae3037f8d67db53713
Christian Grobmeier <grobmeier@gmail.com>
2012-05-15 14:19:43 +0200

    added support for errorhandlers

--- src/roadcrew.js
+++ src/roadcrew.js
@@ -1,3 +1,13 @@
+function RoadcrewError(message, code, rootCause) {
+    this.name = "RoadcrewError";
+    this.message = message || "An error has occured";
+    this.code = code || "1";
+    this.rootCause = rootCause || null;
+}
+
+RoadcrewError.prototype = new Error();
+RoadcrewError.prototype.constructor = RoadcrewError;
+
 function Roadcrew() {
    this.pages = $('.page');
    this.active = $($('.start')[0]);
@@ -13,18 +23,23 @@ Roadcrew.prototype = {
    active : null,
    path : [],
    start: null,
-   interceptor : {}  
+   interceptor : {},
+   globalErrorHandler : null,
+   errorHandler : {}
 };
 
-Roadcrew.prototype.intercept = function (url, interceptor) {
+Roadcrew.prototype.intercept = function (url, interceptor, errorHandler) {
    this.interceptor[url] = interceptor;
+    if(errorHandler !== null && errorHandler !== undefined) {
+        this.errorHandler[url] = errorHandler;
+    }
 };
 
 Roadcrew.createDispatcher = function (target, url) {
    var rd = new RoadcrewDispatcher(target, url);
    var dispatcher = function() {
       rd.dispatch();
-   }
+   };
    return dispatcher;
 };
 
@@ -39,12 +54,23 @@ Roadcrew.prototype.goto = function (event) {
       }
    }
    this.path.push(url);
-   
+
    var interceptor = this.interceptor[url];
 
    if(interceptor !== undefined) {
-      var dispatcher = Roadcrew.createDispatcher(this, url);
-      interceptor(dispatcher);
+       try {
+           var dispatcher = Roadcrew.createDispatcher(this, url);
+           interceptor(dispatcher);
+       } catch (error) {
+           var errorHandler = this.errorHandler[url];
+           if(errorHandler === undefined || errorHandler === null) {
+               if(this.globalErrorHandler !== null) {
+                   this.globalErrorHandler(error);
+               }
+           } else {
+               errorHandler(error);
+           }
+       }
    } else {
       this.flip(url);  
    }
