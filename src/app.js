c3751d7680f2b088aa44f9f0ebbfc23890bd6539
Christian Grobmeier <grobmeier@gmail.com>
2012-11-13 11:13:00 +0100

    global error handler

--- src/app.js
+++ src/app.js
@@ -1,28 +1,35 @@
+$(document).ready(function () {
+    var roadcrew = new Roadcrew();
 
-$(document).ready( function() {
-   var roadcrew = new Roadcrew();
-   
-   roadcrew.intercept('#tablePage', function(dispatch) {
-      roadcrew.flip('#loadingPage');
-      setTimeout(dispatch,1500);
-   });
-   
-   var submit = $('#myform').find('[name="submit"]');
-   submit.click(function(event) {
-      event.preventDefault();
-      roadcrew.goto("#tablePage");
-   });
+    roadcrew.intercept('#tablePage', function (dispatch) {
+        roadcrew.flip('#loadingPage');
+        setTimeout(dispatch, 1500);
+    });
 
-  roadcrew.intercept('#interceptingPage', function(dispatch) {
-      console.log("Intercepting");
-      dispatch();
-  });
+    var submit = $('#myform').find('[name="submit"]');
+    submit.click(function (event) {
+        event.preventDefault();
+        roadcrew.goto("#tablePage");
+    });
 
-  roadcrew.intercept('#troublePage', function(dispatch) {
-      throw new RoadcrewError("I made trouble");
-  }, function(error) {
-      $('#errorPage').find('.error').html(error.message);
-      roadcrew.flip('#errorPage');
-  });
+    roadcrew.intercept('#interceptingPage', function (dispatch) {
+        console.log("Intercepting");
+        dispatch();
+    });
 
+    roadcrew.intercept('#troublePage', function (dispatch) {
+        throw new RoadcrewError("I made trouble");
+    }, function (error) {
+        $('#errorPage').find('.error').html(error.message);
+        roadcrew.flip('#errorPage');
+    });
+
+    roadcrew.intercept('#evenMoreTroublePage', function (dispatch) {
+        throw new RoadcrewError("Even more trouble");
+    });
+
+    roadcrew.globalErrorHandler = function (error) {
+        $('#errorPage').find('.error').html("uncatched error");
+        roadcrew.flip('#errorPage');
+    }
 });
