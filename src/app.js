e9d89c595fe6aa6c50c4875f3cd94485dce3f6ee
Christian Grobmeier <cg@grobmeier.de>
2013-03-28 13:59:13 +0100

    new years copyright update

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 /*
- *  Copyright 2012 Christian Grobmeier
+ *  Copyright 2012-2013 Christian Grobmeier
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
